094bb4c03e6a76f81f31a6c789a4563d9a8b3a13
Thiago Gracini <dev@thiagogracini.com.br>
2025-08-06 17:06:29 -0300

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -10,7 +10,7 @@ function checkPostgres() {
       return;
     }
 
-    console.log("\n🟢 Postgres está pronto e aceitando conexções!\n");
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
   }
 }
 
